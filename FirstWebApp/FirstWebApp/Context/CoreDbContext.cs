8d750f389f0b35b4da20ae5c18dad34964726ac8
Pavel Torchilov <p.torchilov@itransition.com>
2015-07-17 21:38:20 +0300

    Add db context.

---
+++ FirstWebApp/FirstWebApp/Context/CoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Reflection;
+
+namespace FirstWebApp.Context
+{
+    public class CoreDbContext : DbContext
+    {
+        public CoreDbContext() : base("name=DefaultConnection")
+        {
+            Database.SetInitializer<CoreDbContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
