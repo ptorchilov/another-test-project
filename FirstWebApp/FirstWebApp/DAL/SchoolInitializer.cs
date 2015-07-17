b28f186b4b564e3591cc603b9d3a7c5af5dc5546
Pavel Torchilov <p.torchilov@itransition.com>
2015-07-17 22:00:42 +0300

    Add initializer.

---
+++ FirstWebApp/FirstWebApp/DAL/SchoolInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+using Domain.Models;
+
+using FirstWebApp.Context;
+
+namespace FirstWebApp.DAL
+{
+    public class SchoolInitializer : DropCreateDatabaseIfModelChanges<CoreDbContext>
+    {
+        protected override void Seed(CoreDbContext context)
+        {
+            var students = new List<Student>
+                               {
+                                   new Student
+                                       {
+                                           FirstName = Faker.Name.First(),
+                                           LastName = Faker.Name.Last(),
+                                           EnrollmentDate = DateTime.Now
+                                       },
+                                   new Student
+                                       {
+                                           FirstName = Faker.Name.First(),
+                                           LastName = Faker.Name.Last(),
+                                           EnrollmentDate = DateTime.Now
+                                       },
+                                   new Student
+                                       {
+                                           FirstName = Faker.Name.First(),
+                                           LastName = Faker.Name.Last(),
+                                           EnrollmentDate = DateTime.Now
+                                       }
+                               };
+
+            //TODO: Add repositiories for each entities.
+//            students.ForEach(s => context.);
+
+        }
+    }
+}
